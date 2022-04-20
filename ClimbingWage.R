859e4d8b2624339e28cd7a02c4da1195c4cc4f68
Eden Axelrad <46797556+edenaxe@users.noreply.github.com>
2022-04-19 18:26:53 -0700

    Code tidying and optimization

--- ClimbingWage.R
+++ ClimbingWage.R
@@ -1,31 +1,20 @@
 library(tidyverse)
 
 # Load cleaned up version of the raw Climbing Salary Data from Reddit
-Climbing_Wage <- read.csv("~/FTClimbingSalary_Clean.csv") 
-
-
-# Create a summary table for line segment and y-axis label 
-# Shows total range by state and provides count of responses by state
-CW_Table <- Climbing_Wage %>%
-  mutate(Diff = Primary_Clean-MinWage_Clean) %>%
-  group_by(State_Clean) %>%
-  summarise(Count = n(), 
-            Min = min(Primary_Clean), 
-            Mean = mean(Primary_Clean), 
-            Max = max(Primary_Clean),
-            MeanDiff = mean(Diff),
-            MinWage = mean(MinWage_Clean)) %>%
-  mutate(State_N = case_when(
-    Count > 2 ~ paste0(State_Clean, " (N = ", Count, ")"),
-    Count <= 2 ~ "Other (N = 15)"))
-
+# Add count and a state + count (y-axis label) columns 
+Climbing_Wage <- read.csv("FTClimbingSalary_Clean.csv") %>%
+  mutate(Count = map(.x = State_Clean, 
+                     .f = ~table(Climbing_Wage$State_Clean)[.]),
+         State_N = case_when(
+           Count > 2 ~ paste0(State_Clean, " (N = ", parse_number(as.character(Count)), ")"),
+           Count <= 2 ~ "Other (N = 15)"))
 
 # Create the lollipop chart
-CW_Table %>%
+Climbing_Wage %>%
   group_by(State_N) %>%
-  summarise(Min = min(Min), 
-            Mean = mean(Mean), 
-            Max = max(Max)) %>%
+  summarise(Min = min(Primary_Clean), 
+            Mean = mean(Primary_Clean), 
+            Max = max(Primary_Clean)) %>%
   mutate(across(.cols = 2:4, .fns = round, digits = 2),
          State_N = fct_reorder(State_N, Max)) %>%
   arrange(desc(Mean)) %>%
@@ -36,13 +25,12 @@ CW_Table %>%
                    yend = Max), 
                color = "gray", 
                size = 2) +
-  geom_point(data = left_join(Climbing_Wage, CW_Table, by = "State_Clean"),
+  geom_point(data = Climbing_Wage,
              aes(x = State_N, 
                  y = Primary_Clean, 
                  color = Main_Clean),
                  size = 4) +
   scale_color_brewer(palette = "Set2") +
-  ylim(0, 45) +
   coord_flip() +
   scale_y_continuous(limits = c(5, 40), 
                      breaks = (seq(from = 5, to = 40, by = 5)),
