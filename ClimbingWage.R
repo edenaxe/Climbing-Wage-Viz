6765b2541ccf76de21983855dcf1b2b60bafe304
Eden Axelrad <46797556+edenaxe@users.noreply.github.com>
2022-04-21 09:08:18 -0700

    Added interactive plotly version

--- ClimbingWage.R
+++ ClimbingWage.R
@@ -1,40 +1,50 @@
+### Load required libraries ###
+
 library(tidyverse)
 
 
-### Load cleaned up version of the raw Climbing Salary Data from Reddit
-Climbing_Wage <- read.csv("FTClimbingSalary_Clean.csv") %>%
+### Load cleaned up version of the raw Climbing Salary Data from Reddit ###
+
+Climbing_Wage <- read.csv("C:\\Users\\eaxelrad\\Desktop\\Climbing-Wage-Viz-main\\FTClimbingSalary_Clean.csv") %>%
   # Add count column and a state + count (y-axis label) column 
   mutate(Count = map_int(.x = State_Clean, 
                          .f = ~table(State_Clean)[.]),
-         State_N = case_when(
+         State = case_when(
            Count > 2 ~ paste0(State_Clean, " (N = ", Count, ")"),
            Count <= 2 ~ "Other (N = 15)"))
 
 
-### Create the lollipop chart
-Climbing_Wage %>%
-  # Group by "State_N" label column and find min/mean/max for each group
-  group_by(State_N) %>%
+
+### Create the graph ###
+
+CW_Plot <- Climbing_Wage %>%
+  # Group by "State" label column and find min/mean/max for each group
+  group_by(State) %>%
   summarise(Min = min(Primary_Clean), 
             Mean = mean(Primary_Clean), 
             Max = max(Primary_Clean)) %>%
-  # Round numeric columns and reorder "State_N" by maximum wage value
+  # Round numeric columns and reorder "State" by maximum wage value
   mutate(across(.cols = 2:4, .fns = round, digits = 2),
-         State_N = fct_reorder(State_N, Max)) %>%
+         State = fct_reorder(State, Max)) %>%
   # Create the ggplot
   ggplot() +
   # Generate a gray segment that spans from min to max value for each group
-  geom_segment(aes(x = State_N, 
-                   xend = State_N, 
+  geom_segment(aes(x = State, 
+                   xend = State, 
                    y = Min, 
                    yend = Max), 
                color = "gray", 
                size = 2) +
   # Add points along the segment where colors correspond to the primary job type
   geom_point(data = Climbing_Wage,
-             aes(x = State_N, 
+             aes(x = State, 
                  y = Primary_Clean, 
-                 color = Main_Clean),
+                 color = Main_Clean,
+                 # Text element will only be used for the interactive plotly version (ignore warning)
+                 text = paste0("State: ", State_Clean, 
+                               "<br>Primary Job: ", Main_Clean,
+                               "<br>Experience: ", Experience_Clean, " year(s)",
+                               "<br>Hourly Wage: $",  Primary_Clean)),
              size = 3) +
   # Customize the wage axis 
   scale_y_continuous(limits = c(5, 40), 
@@ -60,7 +70,23 @@ Climbing_Wage %>%
        subtitle = "By Primary Job & State",
        color = "Primary Job")
 
+CW_Plot
 
-### Export/save the plot
-ggsave("CW_lollipop.png", units = "in", width = 11, height = 6, dpi = 350)
+
+### Export/save the plot ###
+
+ggsave("CW_Plot.png", units = "in", width = 11, height = 6, dpi = 350)
 dev.off()
+
+
+
+### Create an interactive version ### 
+
+# Generate the interactive version using plotly and call the tooltip from text in geom_point
+plotly::ggplotly(CW_Plot, tooltip = "text") %>%
+  # Remove unwanted buttons
+  plotly::config(modeBarButtonsToRemove = c("pan2d", "autoScale2d", "zoom2d", "zoomIn2d", "zoomOut2d")) %>% 
+  # Remove plotly logo from mode bar
+  plotly::config(displaylogo = FALSE) %>% 
+  # Change the cursor from crosshair to default
+  htmlwidgets::onRender("function(el, x) {Plotly.d3.select('.cursor-crosshair').style('cursor', 'default')}") 
